d9e6deee7ca73bcc75049e7d3084b80737ec2223
umetsu <umetsu.kentaro@adways.net>
2022-07-18 10:33:09 +0900

    :wrench: TypeScript化

--- express_app/src/bin/www.ts
+++ express_app/src/bin/www.ts
@@ -3,23 +3,21 @@
 /**
  * Module dependencies.
  */
+import app from "../app";
+import debugModule from "debug";
+import http from "http";
 
-var app = require('../app');
-var debug = require('debug')('express-app:server');
-var http = require('http');
-
+const debug = debugModule('express-app:server');
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -33,8 +31,8 @@ server.on('listening', onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+function normalizePort(val: any) {
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -53,12 +51,12 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError(error: any) {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -82,9 +80,15 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+
+  if (!addr) {
+    return ""
+  }
+
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
+
   debug('Listening on ' + bind);
 }
